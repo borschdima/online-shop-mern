fd712a4867ccabb8a31d52c8a5b08cd8e6b0deed
Dmytro Borshch <borsch.dima@gmail.com>
2020-04-07 17:15:54 +0300

    Added cart table with items and styled it

--- client/src/pages/Cart/Cart.js
+++ client/src/pages/Cart/Cart.js
@@ -1,15 +1,54 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import SectionHeader from "../../ui/SectionHeader/SectionHeader";
-import { MDBContainer } from "mdbreact";
+import { MDBContainer, MDBDataTable } from "mdbreact";
+import { useHistory } from "react-router-dom";
 
 import "./Cart.scss";
 
 const Cart = () => {
+	const history = useHistory();
+	const cartItems = useSelector((state) => state.cart.items);
+
+	const rowItems = cartItems.map((item) => ({
+		...item,
+		clickEvent() {
+			history.push("/laptops/" + item._id);
+		},
+	}));
+
+	const data = {
+		columns: [
+			{
+				label: "Название",
+				field: "name",
+				sort: "asc",
+			},
+			{
+				label: "Цена",
+				field: "price",
+				sort: "asc",
+			},
+		],
+		rows: rowItems,
+	};
 	return (
 		<section className="cart">
 			<MDBContainer>
 				<SectionHeader title="Корзина" />
 				<p>Здесь будут добавленные товары</p>
+
+				<MDBDataTable
+					striped
+					bordered
+					responsive
+					small
+					btn
+					data={data}
+					infoLabel={["Показывается", "до", "из", "товаров"]}
+					paginationLabel={["Назад", "Вперед"]}
+					searchLabel="Поиск"
+				/>
 			</MDBContainer>
 		</section>
 	);
